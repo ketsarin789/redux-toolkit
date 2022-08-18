f2260d61343cab08b14fdcbc4c9205cf16f3eae1
Ketsarin Kayak <ketsarinkayak@Ketsarins-MacBook-Pro.local>
2022-08-18 13:32:23 -0700

    redux-toolkit

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Products from "../components/Products";
+
+const Home = () => {
+    return(
+        <div>
+        <div>Welcome</div>
+        <Products />
+        </div>
+    )
+}
+export default Home;
